bf8d26437a8c51c9a0381d2eb00e8c588a9b4813
thirdiallo <thirdiallo@gmail.com>
2021-05-08 18:40:47 +0000

    organized projet with multiples modules using

---
+++ src/app/admin/fournisseur/create-fournisseur/create-fournisseur.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-fournisseur',
+  templateUrl: './create-fournisseur.component.html',
+  styleUrls: ['./create-fournisseur.component.scss']
+})
+export class CreateFournisseurComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/customer/commande/add-commande/add-commande.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-commande',
+  templateUrl: './add-commande.component.html',
+  styleUrls: ['./add-commande.component.scss']
+})
+export class AddCommandeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
